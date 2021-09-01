a44c59ead8a7ed0e4050f3f05ff8ce5a3eb07d81
Meet Bariya <marutimeet@gmail.com>
2021-09-01 12:52:28 +0530

    boot type added in variable program

--- 02_variables/numbers.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
-
-	num := 50
-	fmt.Printf("%d %b",num, num)
-}
---
+++ 02_variables/var_using_println.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var pi float64 = 3.1415926
+	var num int = 29
+	var language string = "golang"
+	var available bool = true
+	fmt.Println("pi =", pi)
+	fmt.Println("num =", num)
+	fmt.Println("lang =",language)
+	fmt.Println("available =",available)
+}
